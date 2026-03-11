c973faf05bb68117a3abe00d46f7b2fa9e51a63d
uuur1363-arch <uuur.1363@gmail.com>
2026-03-11 10:39:40 +0300

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    hasUrl: !!process.env.SUPABASE_URL,
-    urlStart: (process.env.SUPABASE_URL || "").slice(0, 20),
-    keyLen: (process.env.SUPABASE_SERVICE_ROLE_KEY || "").length
-  });
-}
